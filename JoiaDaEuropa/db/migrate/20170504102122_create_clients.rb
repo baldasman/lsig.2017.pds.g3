a83af3c56f67cf86f57d2076e83dde44c4abce73
MrMendes <dafy_mendes@hotmail.com>
2017-05-04 12:25:16 +0100

    tabelas db

--- JoiaDaEuropa/db/migrate/20170504102122_create_clients.rb
+++ JoiaDaEuropa/db/migrate/20170504102122_create_clients.rb
@@ -2,12 +2,12 @@ class CreateClients < ActiveRecord::Migration[5.0]
   def change
     create_table :clients do |t|
       t.string :name
-      t.Text :address
+      t.text :address
       t.string :locality
       t.string :email
-      t.Integer :nif
-      t.Text :postal_code
-      t.Integer :contact
+      t.integer :nif
+      t.text :postal_code
+      t.integer :contact
 
 
       t.timestamps
