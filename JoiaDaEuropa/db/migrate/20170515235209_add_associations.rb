5b32f3bc8b1b9f54189ad4cd77ea949c9d193862
MrMendes <dafy_mendes@hotmail.com>
2017-05-16 12:30:25 +0100

    reference

---
+++ JoiaDaEuropa/db/migrate/20170515235209_add_associations.rb
@@ -0,0 +1,9 @@
+class AddAssociations < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :orders, :user, index:true
+    add_reference :states, :order, index:true
+    add_reference :users, :client, index:true
+    add_reference :attachments, :order, index:true
+    add_reference :comments, :order, index:true
+  end
+end
