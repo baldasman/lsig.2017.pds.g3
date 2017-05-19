acbc39e12be575e2e058808390a693d1062f3ea8
Pedro Santos <baldasman@gmail.com>
2017-05-19 10:20:28 +0100

    [PG3-22]

--- JoiaDaEuropa/app/controllers/client_area_controller.rb
+++ JoiaDaEuropa/app/controllers/client_area_controller.rb
@@ -1,37 +1,39 @@
 class ClientAreaController < ApplicationController
 
-  def create_new_order
+    def create_new_order
 
-    @order = Order.new
+        @order = Order.new
 
-  end
-
-  def index
-
-  end
-
-  def save_order
-
-    _order = params[:order]
-
-    @order = Order.new
-
-    @order.name = _order[:name]
-    @order.price = _order[:price]
-    @order.delivery_date = _order[:delivery_date]
-
-    if @order.save
-      redirect_to client_area_view_order_path
-    else
-      redirect_to client_area_create_new_order_path
     end
-  end
+
+    def index
+
+    end
+
+    def save_order
+
+        _order = params[:order]
+
+        @order = Order.new user_id: current_user.id, state_id: 1
+
+        @order.name = _order[:name]
+        @order.price = _order[:price]
+        @order.delivery_date = _order[:delivery_date]
+
+        if @order.save
+            redirect_to client_area_view_order_path
+        else
+            flash[:errors] = @order.errors.messages
+            redirect_to client_area_create_new_order_path
+        end
+    end
 
 
-  def view_order
+    def view_order
 
-    @order = Order.all
+        @order = Order.all
+        @pending_orders = Order.all.where(state_id: 1)
 
-  end
+    end
 end
 
--- JoiaDaEuropa/app/models/order.rb
+++ JoiaDaEuropa/app/models/order.rb
@@ -1,6 +1,9 @@
 class Order < ApplicationRecord
 
-  belongs_to :state
-  belongs_to :user
+    belongs_to :state
+    belongs_to :user
+
+    validates :price, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
 
 end
